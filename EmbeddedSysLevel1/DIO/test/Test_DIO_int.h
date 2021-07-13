59df2fbe0c31a3e1e36fc22eff8f84f2e7b47a29
Elgharib <50600176+abdelrhmanelgharib@users.noreply.github.com>
2021-07-13 13:46:24 +0200

    add Test Case

---
+++ EmbeddedSysLevel1/DIO/test/Test_DIO_int.h
@@ -0,0 +1,20 @@
+/**
+ * @file Test_DIO_int.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-07-13
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+#ifndef TEST_DIO_INT_H_
+#define TEST_DIO_INT_H_
+
+void TestPinValue(void);
+void TestPinDirection(void);
+void TestTogPin(void);
+
+#endif  /* TEST_DIO_INT_H_ */
